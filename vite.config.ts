8e055c1818d1a5f83175cd3a3f7660f0a7ffce32
Lukasz <lukasz.zatyka@gmail.com>
2025-07-20 10:47:11 +0200

    feat: initialize testing environment with Vitest and MSW

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.spec.{ts,tsx}',
+        'src/main.tsx',
+        'src/vite-env.d.ts',
+        'src/types.ts',
+        'src/tests',
+        'src/components/CardSkeleton.component.tsx',
+        'src/index.css',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+      reporter: ['text', 'json', 'html'],
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
