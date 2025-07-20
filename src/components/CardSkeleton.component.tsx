8d3d5ed0cb02d5f6d5df730180867a2291f386b4
Lukasz <lukasz.zatyka@gmail.com>
2025-07-20 16:29:14 +0200

    test: add smoke tests to validate testing environment

--- src/components/CardSkeleton.component.tsx
+++ src/components/CardSkeleton.component.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 class CardSkeleton extends React.Component {
   render() {
-    return <div className="h-38 w-67 bg-gray-300 rounded"></div>;
+    return (
+      <div
+        className="h-38 w-67 bg-gray-300 rounded"
+        data-testid="card-skeleton"
+      ></div>
+    );
   }
 }
 
