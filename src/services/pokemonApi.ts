e0a6ddf2b129dcdb723d1af868b0ca6a2b7a3b98
Lukasz <lukasz.zatyka@gmail.com>
2025-08-08 18:31:59 +0200

    feat: setup RTK Query API slice and integrate with store

---
+++ src/services/pokemonApi.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const pokemonApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  endpoints: (builder) => ({
+    // Endpoints will be defined here
+  }),
+});
